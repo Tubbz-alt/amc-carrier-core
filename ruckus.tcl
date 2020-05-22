e5a7e5ae7eba6967113e5397608fda4b9ac7f496
Matt Weaver <weaver@slac.stanford.edu>
2020-05-22 13:29:37 -0700

    Upgrade for using lcls-timing-core v3.1.0+

--- ruckus.tcl
+++ ruckus.tcl
@@ -35,7 +35,7 @@ set family [getFpgaFamily]
 
 # Check for submodule tagging
 if { [info exists ::env(OVERRIDE_SUBMODULE_LOCKS)] != 1 || $::env(OVERRIDE_SUBMODULE_LOCKS) == 0 } {
-   if { [SubmoduleCheck {lcls-timing-core} {2.0.0} "mustBeExact" ] < 0 } {exit -1}
+   if { [SubmoduleCheck {lcls-timing-core} {3.1.0} ] < 0 } {exit -1}
    if { [SubmoduleCheck {ruckus}           {2.5.0} "mustBeExact" ] < 0 } {exit -1}
    if { [SubmoduleCheck {surf}             {2.5.0} "mustBeExact" ] < 0 } {exit -1}
 } else {
